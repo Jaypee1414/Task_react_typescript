04e7bc52924933e923f6149bf6ff3869ecb770b5
Jaypee1414 <jhnplbncyrn03@gmail.com>
2024-02-05 22:44:46 +0800

    add handleDone

--- src/components/Single.tsx
+++ src/components/Single.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { todo } from '../Model'
+import TodoList from './TodoList';
 type props ={
     todo: todo;
     todos: todo[]; 
@@ -9,13 +10,26 @@ type props ={
 }
 
 function Single({todo, todos, setTodos} : props) {
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo)=>
+            todo.id === id?{...todo, isDone:!todo.isDone}:todo)
+        )
+    }
   return (
     <form className='todos-single'> 
+    {
+        todo.isDone?(
+            <s className='todos-text'>{todo.todo}</s>
+        ):(
+            <s className='todos-text'>{todo.todo}</s>
+        )
+    }
         <span className='todos-text'>{todo.todo}</span>
         <div className=''>
             <span className='icon'>Edit</span>
             <span className='icon'>Delete</span>
-            <span className='icon'>Done</span>
+            <span className='icon' onClick={()=> handleDone(todo.id)}>Done</span>
         </div>
     </form>
   )
